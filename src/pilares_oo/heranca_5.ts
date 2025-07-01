9ef8acdc97a3cce67e0550a428c836c3bcf76b82
caiquemiranda <caiquemirandas@gmail.com>
2025-06-30 22:23:18 -0300

    class abstract class concrect

--- src/pilares_oo/heranca_5.ts
+++ src/pilares_oo/heranca_5.ts
@@ -1,16 +1,32 @@
+// classe abstrata
 abstract class  Celular {
     ligar() {
         console.log('Ligando...');
     }
+
+    abstract accessarRedeMovel(): void;
+
 }
 
-class Iphone23Pro extends Celular {}
-class GalaxyS23Max extends Celular {}
+// classes concretas que herdam de Celular
+class Iphone23Pro extends Celular {
+    accessarRedeMovel(): void {
+        console.log('Acessando rede móvel do iPhone 23 Pro...');
+    }
+}
+
+class GalaxyS23Max extends Celular {
+    accessarRedeMovel(): void {
+        console.log('Acessando rede móvel do Galaxy S23 Max...');
+    }
+}
 
 let c1 = new GalaxyS23Max();
 c1.ligar();
+c1.accessarRedeMovel();
 
 c1 = new Iphone23Pro();
 c1.ligar();
+c1.accessarRedeMovel();
 
 export {}
